3c28111670357b5896aee99c004d6e088e49557b
troncosojuan <troncosojuanbernabe@gmail.com>
2026-02-21 11:11:04 -0300

    fix: vrios

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -16,4 +16,5 @@ export const prisma =
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export * from "@prisma/client";
+export type { Property, PropertyImage, User } from "@prisma/client";
 export default prisma;
